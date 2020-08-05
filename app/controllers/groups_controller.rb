67e60e6d70dd96b8ba144bbc83798b42e90894cd
harumisa <h.a.r.u.ponpon814@gmail.com>
2020-08-06 00:38:12 +0900

    編集したいグループのidから編集画面へ遷移し、編集の上Saveボタンをクリックすると変更した内容が保存されるように編集機能を実装。また、グループの更新が成功した場合にはトップ画面にリダイレクトし「グループを更新しました」というフラッシュメッセージを表示するようにし、更新に失敗した場合には入力内容を残したままグループ編集画面を表示させるように実装。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,11 +6,24 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.create(group_params)
+    @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
     end
   end
 
