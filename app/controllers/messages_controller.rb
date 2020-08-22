92e04873a426107cbb9c03cc1fb8861473dab4b9
harumisa <h.a.r.u.ponpon814@gmail.com>
2020-08-22 12:56:04 +0900

    views/messagesにcreate.json.jbuilderを作成し、JSON形式で返すデータを記述。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,7 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      respone_to do |format|
+      respond_to do |format|
         format.json
       end
     else
